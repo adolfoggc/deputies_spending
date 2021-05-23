ba6a8e2d1a7a67098a7547ab801234dbc32e227a
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-23 19:07:25 -0300

    Insert enum on model Deputy

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,2 +1,9 @@
 class Deputy < ApplicationRecord
+
+	enum federal_unity: {
+		ac: 0, al: 1, am: 2, ap: 3, ba: 4, ce: 5, df: 6, es: 7, go: 8,
+		ma: 10, mg: 11, ms: 12, mt: 13, na: 14, pa: 15, pb: 16, pe: 17, 
+		pi: 18, pr: 19, rj: 20, rn: 21, ro: 22, rr: 23, rs: 24, sc: 25,
+		se: 26, sp: 27, to: 28
+	}
 end
