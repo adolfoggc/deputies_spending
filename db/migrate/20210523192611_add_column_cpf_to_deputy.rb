cebf9c256b6532f7cb9fa0299d80a8eeaf5e52ad
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-23 16:30:56 -0300

    Insert column cpf into Deputy

---
+++ db/migrate/20210523192611_add_column_cpf_to_deputy.rb
@@ -0,0 +1,5 @@
+class AddColumnCpfToDeputy < ActiveRecord::Migration[6.0]
+  def change
+    add_column :deputies, :cpf, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_171057) do
+ActiveRecord::Schema.define(version: 2021_05_23_192611) do
 
   create_table "deputies", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_171057) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "federal_unity"
+    t.string "cpf"
   end
 
 end
