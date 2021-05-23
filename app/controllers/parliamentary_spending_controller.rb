f5843e38cd8a4f9b997acf29f66382a6ceda8824
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-23 19:44:49 -0300

    Update parliamentary_spending#import

--- app/controllers/parliamentary_spending_controller.rb
+++ app/controllers/parliamentary_spending_controller.rb
@@ -21,22 +21,21 @@ class ParliamentarySpendingController < ApplicationController
 				row = row.split(";")
 				while i < row.length do
 					row[i] = row[i].gsub!(/[^0-9A-Za-zÁáÂâÃãÀàÉéÍíÓóÔôÚú]/, '')
-
 					if rows == 0
 						column_titles << row[i]
 					end
 					i += 1
 				end
 
-				if( row[colunas.index("sgUF")]  == "CE")
-					unless cearenses.include?(row[colunas.index("ideCadastro")])
-						cearenses << row[colunas.index("ideCadastro")]
+				if( row[column_titles.index("sgUF")]  == "CE")
+					unless cearenses.include?(row[column_titles.index("ideCadastro")])
+						cearenses << row[column_titles.index("ideCadastro")]
 						Deputy.create(
-							name: to_name(row[colunas.index("txNomeParlamentar")]), 
-							deputy_id: row[colunas.index("ideCadastro")], 
-							party: row[colunas.index("sgPartido")],
-							federal_unity: row[colunas.index("sgUF")].downcase,
-							cpf: row[colunas.index("cpf")]
+							name: to_name(row[column_titles.index("txNomeParlamentar")]), 
+							deputy_id: row[column_titles.index("ideCadastro")], 
+							party: row[column_titles.index("sgPartido")],
+							federal_unity: row[column_titles.index("sgUF")].downcase,
+							cpf: row[column_titles.index("cpf")]
 						)
 					end
 				end
