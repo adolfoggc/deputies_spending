dce22e644d2b534958768e358b4616a0b7b6faa6
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 02:34:31 -0300

    Update import action on parliamentary_spending_controller

--- app/controllers/parliamentary_spending_controller.rb
+++ app/controllers/parliamentary_spending_controller.rb
@@ -8,7 +8,14 @@ class ParliamentarySpendingController < ApplicationController
 		i = 0
 		rows = 0
 		column_titles = []
-		cearenses = []
+		cearenses = Hash.new
+
+		if Invoice.all.count > 0
+			Invoice.all.each do |invoice|
+				invoice.destroy
+			end
+		end
+
 		if Deputy.all.count > 0
 			Deputy.all.each do |deputy|
 				deputy.destroy
@@ -16,11 +23,10 @@ class ParliamentarySpendingController < ApplicationController
 		end
 
 		File.open(myfile).each_line do |row|
-			begin
 				i = 0
 				row = row.split(";")
 				while i < row.length do
-					row[i] = row[i].gsub!(/[^0-9A-Za-zÁáÂâÃãÀàÉéÍíÓóÔôÚú]/, '')
+					row[i] = row[i].gsub!(/[^0-9A-Za-zÁáÂâÃãÀàÉéÍíÓóÔôÚúÇç ]/, '')
 
 					if rows == 0
 						column_titles << row[i]
@@ -29,22 +35,37 @@ class ParliamentarySpendingController < ApplicationController
 				end
 
 				if( row[column_titles.index("sgUF")]  == "CE")
-					unless cearenses.include?(row[column_titles.index("ideCadastro")])
-						
-						cearenses << row[column_titles.index("ideCadastro")]
-						Deputy.create!(
+					unless cearenses.has_key?( row[column_titles.index("ideCadastro")] )
+						new_deputy = Deputy.new(
 							name: row[column_titles.index("txNomeParlamentar")], 
 							deputy_id: row[column_titles.index("ideCadastro")], 
 							party: row[column_titles.index("sgPartido")],
 							federal_unity: row[column_titles.index("sgUF")].downcase,
 							cpf: row[column_titles.index("cpf")]
 						)
+						new_deputy.save
+						
+						cearenses[ row[column_titles.index("ideCadastro")] ] = new_deputy.id
 					end
-				end
 
-			rescue
-			end
+					invoice = Invoice.new(
+						deputy_id: cearenses[ row[column_titles.index("ideCadastro")] ],
+						description: row[column_titles.index("txtDescricao")],
+						specific_description: row[column_titles.index("txtDescricaoEspecificacao")],
+						seller: row[column_titles.index("txtFornecedor")],
+						cnpj_cpf: row[column_titles.index("txtCNPJCPF")],
+						number: row[column_titles.index("txtNumero")],
+						document_kind: row[column_titles.index("indTipoDocumento")].to_i,
+						issue_date: row[column_titles.index("datEmissao")],
+						value: row[column_titles.index("vlrLiquido")]
+					)
+
+					invoice.save!
+				end
 			rows += 1
 		end
+
+		redirect_to deputies_index_path
 	end
+
 end
