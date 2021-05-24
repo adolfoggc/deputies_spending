b933964cb1c462d29eba9f246a4bb112b59a4da2
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 08:34:41 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'deputies/index'
-  get 'deputies/show/:id', to: 'deputies#show', as: "deputies_show"
+  get 'deputies', to: 'deputies#index', as: 'deputies_index'
+  get 'deputies/:id', to: 'deputies#show', as: "deputies_show"
   root 'parliamentary_spending#upload'
   post '/', to: 'parliamentary_spending#import', as: 'import'
 end
