6816f26c541e02882e4d402999e8c848d102393b
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 08:41:50 -0300

    Update controller and view of deputy#show

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -1,8 +1,10 @@
 class DeputiesController < ApplicationController
-  def index
-    @deputies = Deputy.all.order(:name)
-  end
+	def index
+		@deputies = Deputy.all.order(:name)
+	end
 
-  def show
-  end
+	def show
+		@deputy = Deputy.find_by(id: params[:id])
+		@invoices = @deputy.total_in_invoices
+	end
 end
