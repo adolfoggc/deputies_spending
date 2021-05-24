396d3278063589ab9f72576940202445ba2e7ab0
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 02:42:22 -0300

    Create method cpf_format on Deputy model

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -6,4 +6,10 @@ class Deputy < ApplicationRecord
 		pi: 18, pr: 19, rj: 20, rn: 21, ro: 22, rr: 23, rs: 24, sc: 25,
 		se: 26, sp: 27, to: 28
 	}
+
+	def cpf_format
+		self.cpf.insert(3, '.')
+		self.cpf.insert(7, '.')
+		self.cpf.insert(-3, '-')
+	end
 end
