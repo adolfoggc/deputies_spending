71ae4a328e2cc953a0ec79cec27bb3109f7a5672
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 08:35:20 -0300

    Add Invoce relations on Deputy model

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,4 +1,5 @@
 class Deputy < ApplicationRecord
+	has_many :invoices
 
 	enum federal_unity: {
 		ac: 0, al: 1, am: 2, ap: 3, ba: 4, ce: 5, df: 6, es: 7, go: 8,
@@ -12,4 +13,22 @@ class Deputy < ApplicationRecord
 		self.cpf.insert(7, '.')
 		self.cpf.insert(-3, '-')
 	end
+
+	def most_expensive_invoice
+		self.invoices.maximum(:value)
+	end
+
+	def total_in_invoices
+		total = {normal: 0, compensations: 0, mean_by_month: 0}
+		self.invoices.each do |invoice|
+			if invoice.value > 0
+				total[:normal] += invoice.value
+			else
+				total[:compensations] += invoice.value
+			end
+		end
+		total[:mean_by_month] = (total[:normal] + total[:compensations])/12
+		return total
+	end
+
 end
