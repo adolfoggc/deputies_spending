3013c50b4e40f7bef5d4b4d43b405df8cdb93a0d
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 02:36:05 -0300

    Create CPF method on Deputy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'deputies/index'
-  get 'deputies/show'
+  get 'deputies/show/:id', to: 'deputies#show', as: "deputies_show"
   root 'parliamentary_spending#upload'
   post '/', to: 'parliamentary_spending#import', as: 'import'
 end
