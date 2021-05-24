591918a10baeded59af501e0ebfadab90f10d9d0
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 00:46:50 -0300

    Create enum for Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,19 @@
 class Invoice < ApplicationRecord
   belongs_to :deputy
+
+  enum document_kind: {
+    fiscal_document: 0,
+    receipt: 1,
+    foreign_expenses: 2
+  }
+
+  def translate_kind
+    if self.fiscal_document?
+      return "Documento Fiscal"
+    elsif self.receipt?
+      return "Recibo"
+    else
+      return "Despesa no Exterior"
+    end
+  end
 end
