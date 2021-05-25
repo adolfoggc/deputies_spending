4ce37a7aab6fcad1a1b4e5f064e4b7bdea70452f
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-24 21:30:47 -0300

    Create action deputy#ranking

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -7,4 +7,27 @@ class DeputiesController < ApplicationController
 		@deputy = Deputy.find_by(id: params[:id])
 		@invoices = @deputy.total_in_invoices
 	end
+
+	def ranking
+		@deputies_array = []
+		@position = 1
+		count = 0
+		Deputy.all.each do |deputy|
+			invoices = deputy.total_in_invoices
+			deputy_data = {name: deputy.name, total_spent: invoices[:normal] + invoices[:compensations], id: deputy.id}
+			# ordering array
+			ordered = false
+			@deputies_array << deputy_data
+			i = (@deputies_array.length - 1)
+			while !ordered && i > 0
+				if @deputies_array[i][:total_spent] > @deputies_array[i-1][:total_spent]
+					@deputies_array[i], @deputies_array[i-1] = @deputies_array[i-1], @deputies_array[i]
+				elsif @deputies_array[i][:total_spent] <= @deputies_array[i-1][:total_spent]
+					ordered = true
+				end
+				i -= 1
+			end
+		end
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'deputies', to: 'deputies#index', as: 'deputies_index'
+  get 'deputies/ranking', to: 'deputies#ranking', as: "deputies_ranking"
   get 'deputies/:id', to: 'deputies#show', as: "deputies_show"
   root 'parliamentary_spending#upload'
   post '/', to: 'parliamentary_spending#import', as: 'import'
