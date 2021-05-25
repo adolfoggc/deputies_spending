a4dc8f4354d60e1c4e21682bc1c41ebd874f69f5
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-25 10:54:16 -0300

    Add a helper to generate navbar link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def to_real value
     number_to_currency(value, unit: "R$", separator: ",", delimiter: ".")   
   end
+
+  def nav_link text, link, this_controller_name, this_controller_action = nil
+    if this_controller_action.nil? || this_controller_action.blank?
+      link_to text, link, class: "nav-link #{controller.controller_name == this_controller_name ? 'active' : '' }"
+    else
+      link_to text, link, class: "nav-link #{controller.action_name == this_controller_action ? 'active' : '' }"
+    end
+  end
 end
